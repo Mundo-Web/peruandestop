04844c547046c40a89587a2868afcb9e9c0cf2d1
Carlos Colina <carlosecolina89@gmail.com>
2024-05-08 17:27:08 -0500

    actualizacion

--- resources/views/pages/categories/create.blade.php
+++ resources/views/pages/categories/create.blade.php
@@ -89,21 +89,7 @@
                     placeholder="Descripción"></textarea>
                 </div>
               </div>
-              <div class="md:col-span-2">
-                <label for="description">Idioma</label>
-                <select id="tags_id" name="langs"
-                  class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="">Seleccionar Idioma </option>
 
-                  @foreach ($langs as $item)
-                    <option value="{{ $item->code }}">{{ $item->description }}</option>
-                  @endforeach
-
-
-
-
-                </select>
-              </div>
 
 
               <div class="md:col-span-5">
