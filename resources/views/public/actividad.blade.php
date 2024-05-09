70d1080204be14b4d052f82cdbe9612abb632d16
Carlos Colina <carlosecolina89@gmail.com>
2024-05-09 12:46:25 -0500

    idioma por defecto

--- resources/views/public/actividad.blade.php
+++ resources/views/public/actividad.blade.php
@@ -1090,7 +1090,7 @@
               <div class="overflow-hidden rounded-2xl">
                 <div class="img-container ">
                   <img src="{{ asset($item->imagen) }}" alt=""
-                    class=" object-cover w-[800px] h-[300px] rounded-2xl shadow-lg " />
+                    class=" object-cover  h-[300px] rounded-2xl shadow-lg " style="  width: 800px;" />
                 </div>
               </div>
 
