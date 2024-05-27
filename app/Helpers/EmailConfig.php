9249cd71393b4bb70b0b7f4cc24b9a13ac9ce64d
Carlos Colina <carlosecolina89@gmail.com>
2024-05-27 17:03:02 -0500

    actualizada

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -13,13 +13,13 @@ class EmailConfig
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'hola@mundoweb.pe';
-        $mail->Password = 'uohombtocndviqyz';
+        $mail->Username = 'peruandestop@gmail.com';
+        $mail->Password = 'nmsdxxnnjjuvwbog';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
         $mail->Subject = 'Notificación de informacion Recibida';
         $mail->CharSet = 'UTF-8';
-        $mail->setFrom('hola@mundoweb.pe', 'Mundo Web');
+        $mail->setFrom('peruandestop@gmail.com', 'Mundo Web');
         return $mail;
     }
 }
