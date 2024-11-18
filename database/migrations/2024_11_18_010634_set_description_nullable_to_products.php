de58fea80c1b19dd98c339868cc8dfbb2320c33c
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-11-18 01:14:16 -0500

    NTS

--- database/migrations/2024_11_18_010634_set_description_nullable_to_products.php
+++ database/migrations/2024_11_18_010634_set_description_nullable_to_products.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('description')->nullable()->change();
+            $table->longText('description')->nullable()->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('description')->change();
+            $table->longText('description')->change();
         });
     }
 };
